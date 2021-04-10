68c10b76111e2ea805518d75d2ec52fb5c6d405a
Kayte McDonough <kaytemcdonough@gmail.com>
2021-04-10 12:52:42 -0400

    adding a note and adding a new note

--- server.js
+++ server.js
@@ -27,4 +27,24 @@ app.get("/api/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "Develop/db/db.json"));  
   });
 
-  
+// Add a note to the db.json file
+app.post("/api/notes", function(req, res) {
+// Store the body of the request from a frontend in a variable
+var newNote = req.body;
+// Add a unique id to the object to be able to easily access from the db
+newNote.id = uuid.v4();
+
+// Add the new note to the db.json file
+fs.readFile("Develop/db/db.json", function(err, data) {
+    // First have to change the format back into a JS array
+    var notesArr = JSON.parse(data);
+    notesArr.push(newNote);
+    fs.writeFile("Develop/db/db.json", JSON.stringify(notesArr), function(err) {
+    err
+    ? console.log(err)
+    : console.log("Note successfully added to file.")
+    });
+});
+ // Send the newly created note to be displayed on route /api/notes
+ res.json(newNote);
+});
