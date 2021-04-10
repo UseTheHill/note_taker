cd72c2ecf0ece4a1b528399bd25ffcd847b162e3
Kayte McDonough <kaytemcdonough@gmail.com>
2021-04-10 12:51:43 -0400

    added code to show notes page and send the JSON data

--- server.js
+++ server.js
@@ -11,3 +11,20 @@ var PORT = process.env.PORT || 3001;
 app.use(express.static(__dirname + '/Develop/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+//home page
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "Develop/public/index.html"));
+});
+
+//show notes page
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "Develop/public/notes.html"));
+  });
+
+// Send the JSON data containing the notes to be displayed at route /api/notes
+app.get("/api/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "Develop/db/db.json"));  
+  });
+
+  
