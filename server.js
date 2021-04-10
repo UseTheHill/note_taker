65a81a295c5e24f773af22d37fe04bcbfea87de2
Kayte McDonough <kaytemcdonough@gmail.com>
2021-04-10 12:49:17 -0400

    added middleware syntax

--- server.js
+++ server.js
@@ -6,3 +6,8 @@ const uuid = require("uuid");
 const app = express();
 
 var PORT = process.env.PORT || 3001;
+
+// Middleware
+app.use(express.static(__dirname + '/Develop/public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
